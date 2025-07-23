afca5d00075b4a3c50b41d1249c9ba77657ac18f
Sushil Gupta <sushil.codepro@gmail.com>
2025-07-23 22:21:05 +0530

    short url generated

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './src/config/db.js';
+import urlSchema from './src/model/shorturl.model.js';
+import { nanoid } from "nanoid"
 
 const app = express();
 dotenv.config();
@@ -16,6 +18,32 @@ app.get('/', (req, res) => {
     console.log('Response sent');
     res.send('Hi Vedant');
 });
+
+app.post("/api/create/", async (req, res) => {
+  try {
+    console.log('req in create:', req.body);
+
+    const { url } = req.body;
+
+    if (!url) return res.status(400).json({ message: "URL is required" });
+
+    const short_url = nanoid(7);
+
+    const newUrl = new urlSchema({
+      full_url: url,
+      short_url: short_url
+    });
+
+    await newUrl.save();
+
+    res.status(201).json({ message: "Short URL created", short_url });
+
+  } catch (error) {
+    console.error("Error in /api/create:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 
 connectDB();
 // Server
